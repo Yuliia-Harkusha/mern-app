c0488bb765a2a83371b3975c00b0edfabaf3670b
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2023-03-20 15:23:00 +0100

    PlaceList and PlaceItem components

--- src/shared/components/Navigation/SideDrawer.jsx
+++ src/shared/components/Navigation/SideDrawer.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
 import './SideDrawer.css';
 
 const SideDrawer = props => {
-  const content = <aside className="side-drawer">{props.children}</aside>;
+  const content = (
+    <CSSTransition
+      in={props.show}
+      timeout={250}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer" onClick={props.onClick}>
+        {props.children}
+      </aside>
+    </CSSTransition>
+  );
 
   return createPortal(content, document.getElementById('drawer-portal'));
 };
