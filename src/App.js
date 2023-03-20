3bcfef4e670736f0db1e90578187c6406ad795e3
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2023-03-20 13:53:54 +0100

    NavLinks and sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Users from 'user/pages/Users';
+import MainNavigation from 'shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Users />} />
-      <Route path="" element />
-    </Routes>
+    <>
+      <MainNavigation />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="" element />
+        </Routes>
+      </main>
+    </>
   );
 };
 
