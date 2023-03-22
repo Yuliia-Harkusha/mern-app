7c92dd62c0599c5e5328d991777af6d1837ffd29
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2023-03-22 13:43:54 +0100

    UpdatePlace component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Users from 'user/pages/Users';
 import NewPlace from 'places/pages/NewPlace';
 import UserPlaces from 'places/pages/UserPlaces';
 import MainNavigation from 'shared/components/Navigation/MainNavigation';
+import UpdatePlace from 'places/pages/UpdatePlace';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/" element={<Users />} />
           <Route path="/:userId/places" element={<UserPlaces />} />
           <Route path="/places/new" element={<NewPlace />} />
+          <Route path="/places/:placeId" element={<UpdatePlace />} />
         </Routes>
       </main>
     </>
