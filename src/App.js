052ec03a739dd873389607bb95ce9a86e54ab321
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2023-03-23 22:18:03 +0100

    create context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Users from 'user/pages/Users';
 import NewPlace from 'places/pages/NewPlace';
 import UserPlaces from 'places/pages/UserPlaces';
-import MainNavigation from 'shared/components/Navigation/MainNavigation';
 import UpdatePlace from 'places/pages/UpdatePlace';
+import Auth from 'user/pages/Auth';
+import MainNavigation from 'shared/components/Navigation/MainNavigation';
+import { AuthContext } from 'shared/context/auth-context';
 
 const App = () => {
+  const [isLoggedIn, setIsloggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsloggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsloggedIn(false);
+  }, []);
+
   return (
-    <>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
       <MainNavigation />
 
       <main>
@@ -17,9 +31,10 @@ const App = () => {
           <Route path="/:userId/places" element={<UserPlaces />} />
           <Route path="/places/new" element={<NewPlace />} />
           <Route path="/places/:placeId" element={<UpdatePlace />} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </main>
-    </>
+    </AuthContext.Provider>
   );
 };
 
